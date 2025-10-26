d886e959df6c1a87359d76758e884527a482ce23
erwinlumbantoruan1-star <erwinlumbantoruan1@gmail.com>
2025-10-26 15:39:31 +0700

    Create Settings.js

---
+++ Settings.js
@@ -0,0 +1,38 @@
+} 
+global.location = "winzz" // ganti serah mu
+global.ownername = "Zass Onee" // ganti nama mu
+global.ownernumber = '085138373943'  // ganti nomermu
+global.email = "desu.core@zass.id" // gmail.com
+
+//——————————[ Set Bot ]——————————//
+global.botname = 'Neo Ai' // ganti serah mu
+global.versi = '2.0.0'
+global.foot = '© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴢᴀss ᴏғғᴄ' // Footer button
+global.idSaluran = "120363417526801494@newsletter" // idch
+global.namach = "Neo-S Labs Official || Channel" // namach
+global.aiUsageCount = global.aiUsageCount || {}; // jan kacau ha
+
+//——————————[ Set Sticker ]——————————//
+global.packname = ':: Made by Neo - Ai V 2.0.0 ::'
+global.author = `\nForm YT NeoShiroko Labs`
+global.themeemoji = '🪀'
+global.wm = "Neo-S Labs Official"
+// Ganti terserah klean ini pack sticker
+
+//——————————[ Set Link ]——————————//
+global.link = "https://whatsapp.com/channel/0029Vb615brAzNbywHCyRc1w" //linkch
+global.namagc = "Marketplace¹ || Zass Official" //nama grup
+global.linkgc = "https://chat.whatsapp.com/Bvdic3yrpFh5kTkk5oc5G0" //link gc
+global.web = "https://www.neolabsofficial.my.id" // website 
+global.prefa = ['!','.','#','💐']
+
+//——————————[ Set Payment ]——————————//
+global.dana = "Tidak Tersedia" // Isi no dana mu
+global.gopay = "Tidak Tersedia" // Isi no gopay mu
+global.ovo = "Tidak Tersedia" // Isi no ovo mu
+global.qris = "https://link_qr_mu.desu" // pakai fitur .tourl untuk ubah foto ke link
+global.an = {
+  dana: "Nama dana",
+  gopay: "Nama Gopay",
+  ovo: "Nama Ovo"
+}
